1220c89b18a16458c9bdd8f1395e5715858363be
khj <250276857@qq.com>
2018-07-09 20:53:41 +0800

    new add

--- generate3D_data_to_pickle.py
+++ generate3D_data_to_pickle.py
@@ -25,7 +25,7 @@ def resize(input_array):
     output_array = np.array(output_array, dtype='float32')
     return output_array
 
-def gen_3d_volume(ct_array, pt_array, patch_size: int, pixel_spacing:int, ct_save_path, pt_save_path, start_num):
+def gen_3d_volume(ct_array, pt_array, patch_size_h: int, patch_size_w: int, patch_size_d: int, pixel_spacing:int, ct_save_path, pt_save_path, start_num):
     '''
     同时对一个患者的3D pet和ct数据进行裁剪，并转置为[1,h,w,d,c],将图像块对存入对应文件夹的pickle文件中
     :param ct_array: 相同患者全部pet数据生成的ndarray, [1,d,h,w,c]
@@ -37,18 +37,18 @@ def gen_3d_volume(ct_array, pt_array, patch_size: int, pixel_spacing:int, ct_sav
     :param start_num: 图像保存路径起始编号
     :return: pickle文件
     '''
-    height_iter = (ct_array.shape[1] - patch_size) // pixel_spacing + 1
-    width_iter = (ct_array.shape[2] - patch_size) // pixel_spacing + 1
-    depth_iter = (ct_array.shape[3] - patch_size) // pixel_spacing + 1
+    height_iter = (ct_array.shape[1] - patch_size_h) // pixel_spacing + 1
+    width_iter = (ct_array.shape[2] - patch_size_w) // pixel_spacing + 1
+    depth_iter = (ct_array.shape[3] - patch_size_d) // pixel_spacing + 1
 
     temp = 0
     for i in range(height_iter):
         for j in range(width_iter):
             for k in range(depth_iter):
-                temp_ct_array = ct_array[0:, pixel_spacing * i:pixel_spacing * i + patch_size,
-                                pixel_spacing * j:pixel_spacing * j + patch_size, pixel_spacing * k:pixel_spacing * k + patch_size, 0:]
-                temp_pt_array = pt_array[0:, pixel_spacing * i:pixel_spacing * i + patch_size,
-                                pixel_spacing * j:pixel_spacing * j + patch_size, pixel_spacing * k:pixel_spacing * k + patch_size, 0:]
+                temp_ct_array = ct_array[0:, pixel_spacing * i:pixel_spacing * i + patch_size_h,
+                                pixel_spacing * j:pixel_spacing * j + patch_size_w, pixel_spacing * k:pixel_spacing * k + patch_size_d, 0:]
+                temp_pt_array = pt_array[0:, pixel_spacing * i:pixel_spacing * i + patch_size_h,
+                                pixel_spacing * j:pixel_spacing * j + patch_size_w, pixel_spacing * k:pixel_spacing * k + patch_size_d, 0:]
                 ct_save_folder = os.path.join(ct_save_path, '{:>05}.pkl'.format(start_num + temp + 1))
                 pt_save_folder = os.path.join(pt_save_path, '{:>05}.pkl'.format(start_num + temp + 1))
                 with open(ct_save_folder, 'wb') as f:
@@ -72,6 +72,7 @@ def getCTarrayFromFile(file_path):
 
     return ct_array
 
+
 def getPTarrayFromFile(file_path):
     pt_file_path = os.path.join(file_path, 'PT')
     pt_file_names = [os.path.join(pt_file_path, _) for _ in os.listdir(pt_file_path)]
@@ -88,23 +89,25 @@ def main():
     workspace = r'E:\实验数据\2018_05_14_脱敏后PETCT 91例'
     file_paths = [os.path.join(workspace, _) for _ in os.listdir(workspace)]
 
-    ct_save_path = r'E:\training data\3D volume\ct volume'
-    pt_save_path = r'E:\training data\3D volume\pt volume'
+    ct_save_path = r'E:\training data\3D volume(128+128)\ct volume'
+    pt_save_path = r'E:\training data\3D volume(128+128)\pt volume'
     if not os.path.exists(ct_save_path):
         os.mkdir(ct_save_path)
     if not os.path.exists(pt_save_path):
         os.mkdir(pt_save_path)
 
     start_num = 0
-    pixel_spacing = 32
+    pixel_spacing = 10
+    patch_size_h = 128
+    patch_size_w = 128
+    patch_size_d = 64
     file_num = len(file_paths)
     for i in range(file_num):
         print('processing {}/{}:   {}'.format(i, file_num, file_paths[i]))
         print('start_num:  {}'.format(start_num))
         ct_array = getCTarrayFromFile(file_paths[i])
         pt_array = getPTarrayFromFile(file_paths[i])
-        start_num += gen_3d_volume(ct_array, pt_array, 64, pixel_spacing, ct_save_path, pt_save_path, start_num)
-
+        start_num += gen_3d_volume(ct_array, pt_array, patch_size_h, patch_size_w, patch_size_d, pixel_spacing, ct_save_path, pt_save_path, start_num)
 
 if __name__ == '__main__':
     main()
